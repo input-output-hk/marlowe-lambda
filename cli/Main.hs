d21470de506f9d17c27abb882d2641e17b42aa7b
Brian W Bush <brian.bush@iohk.io>
2022-10-29 20:09:08 -0600

    SCP-4614 Improved error reporting.

--- cli/Main.hs
+++ cli/Main.hs
@@ -1,26 +1,26 @@
 
 
-{-# LANGUAGE OverloadedStrings #-}
-
-
 module Main (
   main
 ) where
 
 
-import Data.Aeson ((.=), decode, encode, object)
+import Data.Aeson (eitherDecode, encode)
 import Data.Default (def)
 import Language.Marlowe.Lambda (handle)
+import System.Exit (die)
 
-import Data.ByteString.Lazy.Char8 as LBS8 (getContents, putStr)
+import qualified Data.ByteString.Lazy.Char8 as LBS8 (getContents, putStrLn, unpack)
 
 
 main :: IO ()
 main =
    do
-     Just input <- decode <$> LBS8.getContents
-     result <- handle input def
-     LBS8.putStr . encode
-       $ case result of
-           Right msg -> object ["success" .= msg]
-           Left  msg -> object ["failure" .= msg]
+     input' <- eitherDecode <$> LBS8.getContents
+     case input' of
+       Left msg    -> die msg
+       Right input -> do
+                        result <- handle input def
+                        case result of
+                          Right msg -> LBS8.putStrLn $ encode msg
+                          Left  msg -> die . LBS8.unpack $ encode msg

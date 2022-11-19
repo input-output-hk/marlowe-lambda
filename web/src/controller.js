2b07e22d8ae04044748610798d5e931d8d628760
Brian W Bush <code@functionally.io>
2022-11-19 13:10:39 -0700

    SCP-4725 Separated pages for party and counterparty.

--- web/src/controller.js
+++ web/src/controller.js
@@ -9,53 +9,63 @@ import * as Lambda from "./lambda"
 //import * as renderjson from "renderjson"
 
 
+export const lambda = Lambda
+
+
 const second = 1000
 
 
-var contractId = "26e8839a5674df7059bf3427eb670d6985632df99859e3c14b012d39bba989e5#1"
+var contractId = null
+
+
+export async function unfollowAll() {
+  Lambda.followedContracts(async function(response) {
+  await Promise.all([response.contractIds.map(id => Lambda.unfollowContract(id, function() {}))])
+  })
+}
 
 
 function makeContract(addressA, currencyA, tokenA, amountA, addressB, currencyB, tokenB, amountB, timeout = 1669918020000) {
   return {
     when: [
       {
-	case: {
-	  party: { address:addressA },
-	  deposits: amountA,
-	  of_token: { token_name: tokenA, currency_symbol:currencyA },
-	  into_account: { address: addressA },
-	},
-	then: {
-	  when: [
-	    {
-	      case: {
-		party: { address: addressB },
-		deposits: amountB,
-		of_token: { token_name: tokenB, currency_symbol:currencyB },
-		into_account: { address: addressB },
-	      },
-	      then: {
-		pay: amountB,
-		token: { token_name: tokenB, currency_symbol:currencyB },
-		from_account: { address: addressB },
-		to: {
-		  party: { address:addressA },
-		},
-		then: {
-		  pay: amountA,
-		  token: { token_name: tokenA, currency_symbol: currencyA },
-		  from_account: { address:addressA },
-		  to: {
-		    party: { address:addressB },
-		  },
-		  then: "close",
-		},
-	      },
-	    },
-	  ],
-	  timeout_continuation: "close",
-	  timeout:timeout,
-	},
+        case: {
+          party: { address:addressA },
+          deposits: amountA,
+          of_token: { token_name: tokenA, currency_symbol:currencyA },
+          into_account: { address: addressA },
+        },
+        then: {
+          when: [
+            {
+              case: {
+                party: { address: addressB },
+                deposits: amountB,
+                of_token: { token_name: tokenB, currency_symbol:currencyB },
+                into_account: { address: addressB },
+              },
+              then: {
+                pay: amountB,
+                token: { token_name: tokenB, currency_symbol:currencyB },
+                from_account: { address: addressB },
+                to: {
+                  party: { address:addressA },
+                },
+                then: {
+                  pay: amountA,
+                  token: { token_name: tokenA, currency_symbol: currencyA },
+                  from_account: { address:addressA },
+                  to: {
+                    party: { address:addressB },
+                  },
+                  then: "close",
+                },
+              },
+            },
+          ],
+          timeout_continuation: "close",
+          timeout:timeout,
+        },
       }
     ],
     timeout_continuation: "close",
@@ -64,7 +74,7 @@ function makeContract(addressA, currencyA, tokenA, amountA, addressB, currencyB,
 }
 
 
-export async function createContract() {
+export async function createContract(single) {
   const contract = makeContract(
     uiAddressA.value, uiCurrencyA.value, uiTokenA.value, parseInt(uiAmountA.value),
     uiAddressB.value, uiCurrencyB.value, uiTokenB.value, parseInt(uiAmountB.value),
@@ -90,7 +100,7 @@ export async function createContract() {
   uiCreate.disabled = true
 }
 
-export async function partyDeposit() {
+export async function partyDeposit(single) {
   deposit(
     uiAddressA.value,
     uiKeyA.value,
@@ -99,12 +109,19 @@ export async function partyDeposit() {
     parseInt(uiAmountA.value),
     1,
     uiTxId2,
-    function() {uiCounterpartyDeposit.disabled = false},
+    function() {
+      if (window.uiCounterpartyDeposit != null)
+	uiCounterpartyDeposit.disabled = false
+      else
+	updateConfirmed(2, function(response) {
+	  showTxId(uiTxId3, response.steps[1].txId)
+	})
+    },
   )
   uiPartyDeposit.disabled = true
 }
 
-export async function counterpartyDeposit() {
+export async function counterpartyDeposit(single) {
   deposit(
     uiAddressB.value,
     uiKeyB.value,
@@ -113,9 +130,13 @@ export async function counterpartyDeposit() {
     parseInt(uiAmountB.value),
     2,
     uiTxId3,
-    function () {uiCreate.disabled = false},
+    function () {
+      if (window.uiCreate != null)
+	uiCreate.disabled = false
+    },
   )
-  uiCounterpartyDeposit.disabled = true
+  if (window.uiCounterpartyDeposit != null)
+    uiCounterpartyDeposit.disabled = true
 }
 
 
@@ -158,11 +179,11 @@ async function transact(response1, key, steps, uiTxId, f, g) {
     Lambda.submit(response2.tx, function(response3) {
       console.log(response3)
       const txId = response3.txId
-      uiTxId.innerHTML = '<a href="' + window.secrets.cardanoscalUrl + '/transaction/' + txId + '?tab=utxo" target="marloweSwap">' + txId + '</a>'
+      showTxId(uiTxId, txId)
       f(txId)
-      updateConfirmed(steps, function() {
+      updateConfirmed(steps, function(response) {
         document.body.style.cursor = "default"
-	g()
+        g()
       })
     })
   })
@@ -170,42 +191,108 @@ async function transact(response1, key, steps, uiTxId, f, g) {
 
 async function updateConfirmed(steps, f) {
   Lambda.getContract(contractId, async function(response) {
-    if (response == "Contract not found." || response.steps.length < steps) {
+    if (response == "Contract not found." || response.steps == null || response.steps.length < steps) {
       setTimeout(function(){updateConfirmed(steps, f)}, 5000)
     } else {
       window.renderjson.set_show_to_level(3)
       delete response.response
       uiOutput.replaceChildren(window.renderjson(response))
-      f()
+      if (uiAddressA.readOnly && steps == 0) {
+        uiAddressA.value = response.creation.output.datum.marloweContract.when[0].case.into_account.address
+        uiCurrencyA.value = response.creation.output.datum.marloweContract.when[0].case.of_token.currency_symbol
+        uiTokenA.value = response.creation.output.datum.marloweContract.when[0].case.of_token.token_name
+	uiAmountA.value = response.creation.output.datum.marloweContract.when[0].case.deposits
+        uiCurrencyB.value = response.creation.output.datum.marloweContract.when[0].then.when[0].case.of_token.currency_symbol
+        uiTokenB.value = response.creation.output.datum.marloweContract.when[0].then.when[0].case.of_token.token_name
+	uiAmountB.value = response.creation.output.datum.marloweContract.when[0].then.when[0].case.deposits
+      }
+      f(response)
     }
   })
 }
 
 
-export async function initialize() {
+async function discoverFollowed(f) {
+  Lambda.followedContracts(function(response) {
+    if (response.contractIds.length == 0)
+      setTimeout(function(){discoverFollowed(f)}, 10000)
+    else
+      f(response.contractIds[0])
+  })
+}
 
-  uiAddressA.value = window.secrets.party.address
-  uiKeyA.value = window.secrets.party.key
-  uiCurrencyA.value = window.secrets.party.currency
-  uiTokenA.value = window.secrets.party.token
-  uiAmountA.value = window.secrets.party.amount
+
+export async function initialize(single) {
+
+  if (window.uiKeyA != null) {
+    uiAddressA.value = window.secrets.party.address
+    uiKeyA.value = window.secrets.party.key
+    uiCurrencyA.value = window.secrets.party.currency
+    uiTokenA.value = window.secrets.party.token
+    uiAmountA.value = window.secrets.party.amount
+    uiCurrencyB.value = window.secrets.counterparty.currency
+    uiTokenB.value = window.secrets.counterparty.token
+    uiAmountB.value = window.secrets.counterparty.amount
+  } else {
+    uiAddressA.readOnly = true
+    uiCurrencyA.readOnly = true
+    uiTokenA.readOnly = true
+    uiAmountA.readOnly = true
+    uiCurrencyB.readOnly = true
+    uiTokenB.readOnly = true
+    uiAmountB.readOnly = true
+  }
 
   uiAddressB.value = window.secrets.counterparty.address
-  uiKeyB.value = window.secrets.counterparty.key
-  uiCurrencyB.value = window.secrets.counterparty.currency
-  uiTokenB.value = window.secrets.counterparty.token
-  uiAmountB.value = window.secrets.counterparty.amount
+  if (window.uiKeyB != null) {
+    uiKeyB.value = window.secrets.counterparty.key
+  }
 
-  reset()
+  reset(single)
 
 }
 
 
-export async function reset() {
+function showTxId(uiTxId, txId) {
+  uiTxId.innerHTML = '<a href="' + window.secrets.cardanoscanUrl + '/transaction/' + txId + '?tab=utxo" target="marloweSwap">' + txId + '</a>'
+}
+
+
+export async function reset(single) {
+
   uiNetwork.innerHTML = window.secrets.network
   uiOutput.innerHTML = ""
-  uiCreate.disabled = false
-  uiPartyDeposit.disabled = true
-  uiCounterpartyDeposit.disabled = true
+
+  contractId = ""
+  uiContractId.innerHTML = ""
+  uiTxId1.innerHTML = ""
+  uiTxId2.innerHTML = ""
+  uiTxId3.innerHTML = ""
+
+  if (window.uiCreate != null) {
+    uiCreate.disabled = false
+    uiPartyDeposit.disabled = true
+  }
+  if (window.uiCounterpartyDeposit != null)
+    uiCounterpartyDeposit.disabled = true
+
   document.body.style.cursor = "default"
+
+  if (single) {
+    if (window.uiCreate != null) {
+      await unfollowAll()
+    } else {
+      discoverFollowed(async function(id) {
+        contractId = id
+        uiContractId.innerHTML = contractId
+	showTxId(uiTxId1, id.slice(0, -2))
+	updateConfirmed(0, async function(response1) {
+	  updateConfirmed(1, async function(response2) {
+	    showTxId(uiTxId2, response2.steps[0].txId)
+	    uiCounterpartyDeposit.disabled = false
+	  })
+	})
+      })
+    }
+  }
 }

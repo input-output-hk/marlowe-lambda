85a188aa2c17037eee569796fcc675de3e294c97
Brian W Bush <brian.bush@iohk.io>
2023-01-05 13:56:11 -0700

    SCP-4846 Added command-line flags to CLI tool.

--- cli/Main.hs
+++ cli/Main.hs
@@ -9,27 +9,35 @@ module Main (
 
 
 import Data.Aeson (eitherDecode, encode)
-import Data.Default (def)
 import Language.Marlowe.Runtime.App (handle)
-import System.Environment (getArgs)
-import System.Exit (die)
+import Language.Marlowe.Runtime.App.Parser (getConfigParser)
 
-import qualified Data.ByteString.Lazy.Char8 as LBS8 (getContents, putStrLn, unpack)
+import qualified Data.ByteString.Lazy.Char8 as LBS8 (getContents, putStrLn, lines)
+import qualified Options.Applicative as O
 
 
 main :: IO ()
 main =
-   do
-     config <-
-       getArgs >>= \case
-         []     -> pure def
-         [file] -> read <$> readFile file
-         _      -> die "Invalid argument."
-     LBS8.getContents
-       >>= (
-         \case
-           Left msg    -> die msg
-           Right input -> handle config input >>= \case
-                            Right msg -> LBS8.putStrLn $ encode msg
-                            Left  msg -> die . LBS8.unpack $ encode msg
-       ) . eitherDecode
+  do
+    configParser <- getConfigParser
+    config <-
+      O.execParser
+        $ O.info
+          (O.helper {- <*> O.versionOption -} <*> configParser)
+          (
+            O.fullDesc
+              <> O.progDesc "This command-line tool reads lines of JSON from standard input, interpets them as Marlowe App requests, executes them, and prints the response JSON on standard output."
+              <> O.header "marlowe-lambda-cli: run marlowe application requests"
+          )
+    requests <- LBS8.lines <$> LBS8.getContents
+    sequence_
+      [
+        case eitherDecode line of
+          Right request -> handle config request
+                             >>= \case
+                              Right response -> LBS8.putStrLn $ encode response
+                              Left message -> LBS8.putStrLn $ encode message
+          Left message -> LBS8.putStrLn $ encode message
+      |
+        line <- requests
+      ]

9bee9ab9038102631631e5ab4e095bbf1525e29b
Brian W Bush <brian.bush@iohk.io>
2023-01-05 14:22:29 -0700

    SCP-4846 Added event logging to `marlowe-lambda-cli`.

--- cli/Main.hs
+++ cli/Main.hs
@@ -1,6 +1,7 @@
 
 
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 
 module Main (
@@ -8,11 +9,18 @@ module Main (
 ) where
 
 
+import Control.Monad.Except (liftIO, runExceptT)
 import Data.Aeson (eitherDecode, encode)
-import Language.Marlowe.Runtime.App (handle)
+import Data.Text (Text)
 import Language.Marlowe.Runtime.App.Parser (getConfigParser)
+import Language.Marlowe.Runtime.App.Transact (handleWithEvents)
+import Observe.Event (addField, hoistEvent, withEvent)
+import Observe.Event.Dynamic (DynamicEventSelector(..))
+import Observe.Event.Render.JSON (DefaultRenderSelectorJSON(defaultRenderSelectorJSON))
+import Observe.Event.Render.JSON.Handle (simpleJsonStderrBackend)
+import Observe.Event.Syntax ((≔))
 
-import qualified Data.ByteString.Lazy.Char8 as LBS8 (getContents, putStrLn, lines)
+import qualified Data.ByteString.Lazy.Char8 as LBS8 (getContents, putStrLn, lines, unpack)
 import qualified Options.Applicative as O
 
 
@@ -29,15 +37,20 @@ main =
               <> O.progDesc "This command-line tool reads lines of JSON from standard input, interpets them as Marlowe App requests, executes them, and prints the response JSON on standard output."
               <> O.header "marlowe-lambda-cli: run marlowe application requests"
           )
+    eventBackend <- simpleJsonStderrBackend defaultRenderSelectorJSON
     requests <- LBS8.lines <$> LBS8.getContents
     sequence_
       [
-        case eitherDecode line of
-          Right request -> handle config request
-                             >>= \case
-                              Right response -> LBS8.putStrLn $ encode response
-                              Left message -> LBS8.putStrLn $ encode message
-          Left message -> LBS8.putStrLn $ encode message
+        withEvent eventBackend (DynamicEventSelector "MarloweApp")
+          $ \event ->
+            do
+              addField event $ ("line" :: Text) ≔ LBS8.unpack line
+              case eitherDecode line of
+                Right request -> runExceptT (handleWithEvents (hoistEvent liftIO event) "Handle" config request pure)
+                                   >>= \case
+                                    Right response -> LBS8.putStrLn $ encode response
+                                    Left message -> LBS8.putStrLn $ encode message
+                Left message -> LBS8.putStrLn $ encode message
       |
         line <- requests
       ]
